47ac1a871cb1d323be22ae62a36c0ae30adcb9cf
18615659309 <chen_yueling2021>
2021-06-07 17:26:16 +0800

    会员卡类别service提交

--- shiguang-manager/src/main/java/com/shiguang/baseinfomation/service/impl/CardTypeServiceImpl.java
+++ shiguang-manager/src/main/java/com/shiguang/baseinfomation/service/impl/CardTypeServiceImpl.java
@@ -1,4 +1,51 @@
 package com.shiguang.baseinfomation.service.impl;
 
-public class CardTypeServiceImpl {
+import com.shiguang.baseinfomation.dao.CardTypeDao;
+import com.shiguang.baseinfomation.domain.CardTypeDO;
+import com.shiguang.baseinfomation.service.CardTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CardTypeServiceImpl implements CardTypeService {
+    @Autowired
+    private CardTypeDao typeDao;
+
+    @Override
+    public CardTypeDO get(Long id){
+        return typeDao.get(id);
+    }
+
+    @Override
+    public List<CardTypeDO> list(Map<String, Object> map){
+        return typeDao.list(map);
+    }
+
+    @Override
+    public int count(Map<String, Object> map){
+        return typeDao.count(map);
+    }
+
+    @Override
+    public int save(CardTypeDO type){
+        return typeDao.save(type);
+    }
+
+    @Override
+    public int update(CardTypeDO type){
+        return typeDao.update(type);
+    }
+
+    @Override
+    public int remove(Long id){
+        return typeDao.remove(id);
+    }
+
+    @Override
+    public int batchRemove(Long[] ids){
+        return typeDao.batchRemove(ids);
+    }
 }
